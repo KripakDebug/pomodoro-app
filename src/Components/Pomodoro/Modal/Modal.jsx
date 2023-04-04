00f15280ccd4613805e95a454d68828a8a80bb78
KripakDebug <kastrula123@gmail.com>
2023-04-04 20:29:11 +0300

    Refactor input Modal

--- src/Components/Pomodoro/Modal/Modal.jsx
+++ src/Components/Pomodoro/Modal/Modal.jsx
@@ -8,15 +8,15 @@ const ModalWindow = (props) => {
             <form className={s.form} onSubmit={props.onSubmit}>
                 <div>
                     <label htmlFor="focusTime">Focus</label>
-                    <InputNumber id={'focusTime'} type="number"/>
+                    <InputNumber min={1} id={'focusTime'} type="number"/>
                 </div>
                 <div>
                     <label htmlFor="relaxTime">Short Brake</label>
-                    <InputNumber id={'relaxTime'} type="number"/>
+                    <InputNumber min={1} id={'relaxTime'} type="number"/>
                 </div>
                 <div>
                     <label htmlFor="longBrake">Long Brake</label>
-                    <InputNumber id={'longBrake'} type="number"/>
+                    <InputNumber min={1} id={'longBrake'} type="number"/>
                 </div>
                 <button onClick={props.handleOk}>Submit</button>
             </form>
