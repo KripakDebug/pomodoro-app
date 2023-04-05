255fdd3f0bec0a0a2b842f9710c778c02d585ab5
KripakDebug <kastrula123@gmail.com>
2023-04-05 23:45:47 +0300

    Refactor function and useState

--- src/Components/Pomodoro/Pomodoro.jsx
+++ src/Components/Pomodoro/Pomodoro.jsx
@@ -1,7 +1,5 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import s from './Pomodoro.module.css';
-import timerSound from './../../audio/timergo.mp3';
-import click from './../../audio/click.mp3';
 import {Helmet} from "react-helmet";
 import favicon from './../../icons/favicon.ico';
 import faviconGreen from './../../icons/favicon-green.ico';
@@ -11,50 +9,19 @@ import Timer from "../Timer/Timer";
 import ModalWindow from "../Modal/Modal";
 import classNames from "classnames";
 function Pomodoro() {
-
-    const [running, setRunning] = useState(false);
-    const [intervalId, setIntervalId] = useState(null);
     const [focus, setFocus] = useState(1500);
     const [shortBrake, setShortBrake] = useState(300);
     const [longBrake, setLongBrake] = useState(900);
     const [timer, setTimer] = useState(focus);
-    const [currentInterval, setCurrentInterval] = useState('focus');
-    const [breakCount, setBreakCount] = useState(0);
     const [pauseTimer, setPauseTimer] = useState(false);
+    const [currentInterval, setCurrentInterval] = useState('focus');
+
     const classes = classNames([
         s.wrapper,
         {[s.focus]: currentInterval === 'focus'},
         {[s.relax]: currentInterval === 'relax'},
         {[s.longRelax]: currentInterval === 'longRelax'},
     ]);
-    useEffect(() => {
-        document.title = `${formatTime(timer)} - Time for a focus!`;
-        if (timer === 0) {
-            new Audio(timerSound).play()
-            stopTimer()
-            setPauseTimer(false);
-            if (currentInterval === 'focus') {
-                setCurrentInterval('relax');
-                setTimer(shortBrake);
-                setBreakCount(count => count + 1);
-            } else {
-                setCurrentInterval('focus');
-                setTimer(focus);
-            }
-        }
-        if (currentInterval === 'relax' || currentInterval === 'longRelax') {
-            document.title = `${formatTime(timer)} - Time for a break!`;
-        }
-    }, [timer, currentInterval]);
-
-
-    useEffect(() => {
-        if (breakCount === 4) {
-            setCurrentInterval('longRelax');
-            setTimer(longBrake);
-            setBreakCount(0);
-        }
-    }, [breakCount]);
 
     return (
         <div className={classes}>
@@ -69,55 +36,14 @@ function Pomodoro() {
                     {pauseTimer === true && <link rel="icon" href={faviconGray} type="image/x-icon"/>}
                 </Helmet>
                 <ModalWindow onSubmit={onSubmit}/>
-                <Timer formatTime={formatTime} timer={timer} running={running}
-                       startTimer={startTimer} stopTimer={stopTimer} nextTimer={nextTimer}
-                       clearTimer={clearTimer} onClickTimer={onClickTimer} currentInterval={currentInterval}/>
+                <Timer setPauseTimer={setPauseTimer} timer={timer} setTimer={setTimer}
+                       shortBrake={shortBrake} focus={focus} longBrake={longBrake}
+                       setCurrentInterval={setCurrentInterval} currentInterval={currentInterval}/>
             </div>
         </div>
     )
 
-    function startTimer() {
-        setPauseTimer(false)
-        new Audio(click).play()
-        setIntervalId(setInterval(() => {
-            setTimer(time => time - 1)
-        }, 10));
-        setRunning(true);
-    };
 
-    function nextTimer() {
-        stopTimer()
-        setPauseTimer(false);
-        if (currentInterval === "focus") {
-            setCurrentInterval('relax');
-            setTimer(shortBrake);
-            setBreakCount(count => count + 1);
-            if (breakCount === 4) {
-                setCurrentInterval('longRelax');
-                setTimer(longBrake);
-            }
-        } else {
-            setCurrentInterval('focus');
-            setTimer(focus);
-        }
-    };
-
-    function stopTimer() {
-        new Audio(click).play()
-        clearInterval(intervalId);
-        setPauseTimer(true);
-        setIntervalId(null);
-        setRunning(false)
-
-    };
-
-    function clearTimer() {
-        stopTimer()
-        setPauseTimer(false);
-        setBreakCount(0);
-        setTimer(focus)
-        setCurrentInterval('focus');
-    };
 
     function onSubmit(e) {
         e.preventDefault()
@@ -143,30 +69,6 @@ function Pomodoro() {
         }
     };
 
-    function formatTime(timeInSeconds) {
-        const minutes = Math.floor(timeInSeconds / 60).toString().padStart(2, '0');
-        const seconds = (timeInSeconds % 60).toString().padStart(2, '0');
-        return `${minutes}:${seconds}`;
-    };
-    function onClickTimer(interval) {
-        stopTimer()
-        setPauseTimer(false);
-        setCurrentInterval(interval);
-
-        switch (interval) {
-            case 'focus':
-                setTimer(focus);
-                break;
-            case 'relax':
-                setTimer(shortBrake);
-                break;
-            case 'longRelax':
-                setTimer(longBrake);
-                break;
-            default:
-                break;
-        }
-    };
 }
 
 
--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import s from "../Pomodoro/Pomodoro.module.css";
 import {StepForwardOutlined, UndoOutlined} from "@ant-design/icons";
+import click from "../../audio/click.mp3";
+import timerSound from "../../audio/timergo.mp3";
 
-function Timer({onClickTimer, currentInterval,formatTime, timer, running, startTimer, stopTimer, nextTimer, clearTimer}) {
+function Timer({currentInterval, timer, focus, shortBrake, longBrake, setCurrentInterval, setTimer, setPauseTimer}) {
+    const [running, setRunning] = useState(false);
+    const [intervalId, setIntervalId] = useState(null);
+    const [breakCount, setBreakCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `${formatTime(timer)} - Time for a focus!`;
+        if (timer === 0) {
+            new Audio(timerSound).play()
+            stopTimer()
+            setPauseTimer(false);
+            if (currentInterval === 'focus') {
+                setCurrentInterval('relax');
+                setTimer(shortBrake);
+                setBreakCount(count => count + 1);
+            } else {
+                setCurrentInterval('focus');
+                setTimer(focus);
+            }
+        }
+        if (currentInterval === 'relax' || currentInterval === 'longRelax') {
+            document.title = `${formatTime(timer)} - Time for a break!`;
+        }
+    }, [timer, currentInterval]);
+
+
+    useEffect(() => {
+        if (breakCount === 4) {
+            setCurrentInterval('longRelax');
+            setTimer(longBrake);
+            setBreakCount(0);
+        }
+    }, [breakCount]);
 
     return (
         <div className={s.timer}>
@@ -33,6 +67,75 @@ function Timer({onClickTimer, currentInterval,formatTime, timer, running, startT
             </ul>
         </div>
     )
+
+
+    function startTimer() {
+        setPauseTimer(false)
+        new Audio(click).play()
+        setIntervalId(setInterval(() => {
+            setTimer(time => time - 1)
+        }, 1000));
+        setRunning(true);
+    };
+
+    function nextTimer() {
+        stopTimer()
+        setPauseTimer(false);
+        if (currentInterval === "focus") {
+            setCurrentInterval('relax');
+            setTimer(shortBrake);
+            setBreakCount(count => count + 1);
+            if (breakCount === 4) {
+                setCurrentInterval('longRelax');
+                setTimer(longBrake);
+            }
+        } else {
+            setCurrentInterval('focus');
+            setTimer(focus);
+        }
+    };
+
+    function stopTimer() {
+        new Audio(click).play()
+        clearInterval(intervalId);
+        setPauseTimer(true);
+        setIntervalId(null);
+        setRunning(false)
+
+    };
+
+    function clearTimer() {
+        stopTimer()
+        setPauseTimer(false);
+        setBreakCount(0);
+        setTimer(focus)
+        setCurrentInterval('focus');
+    };
+
+    function formatTime(timeInSeconds) {
+        const minutes = Math.floor(timeInSeconds / 60).toString().padStart(2, '0');
+        const seconds = (timeInSeconds % 60).toString().padStart(2, '0');
+        return `${minutes}:${seconds}`;
+    };
+    function onClickTimer(interval) {
+        stopTimer()
+        setPauseTimer(false);
+        setCurrentInterval(interval);
+
+        switch (interval) {
+            case 'focus':
+                setTimer(focus);
+                break;
+            case 'relax':
+                setTimer(shortBrake);
+                break;
+            case 'longRelax':
+                setTimer(longBrake);
+                break;
+            default:
+                break;
+        }
+    };
 }
 
 
